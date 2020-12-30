8fd404ce7523b9e4475cd5b0cf7057e8454f0ae3
lei95123 <leo95123@qq.com>
2020-12-30 22:13:21 +0800

    feat:已完成,缺动画

--- components/modal/index.js
+++ components/modal/index.js
@@ -1,46 +1,27 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+import "./index.scss";
 import {
-  CheckCircleOutlined,
-  InfoCircleOutlined,
-  CloseCircleOutlined,
-  ExclamationCircleOutlined,
+  CheckCircleTwoTone,
+  InfoCircleTwoTone,
+  CloseCircleTwoTone,
+  ExclamationCircleTwoTone,
   CloseOutlined,
 } from "@ant-design/icons";
 
 // Icon
 const Icon = (props) => {
-  switch (props.icon) {
+  switch (props.type) {
     case "success":
-      return <CheckCircleOutlined twoToneColor="#52c41a" />;
+      return <CheckCircleTwoTone twoToneColor="#52c41a" />;
     case "info":
-      return <InfoCircleOutlined twoToneColor="#1890ff" />;
+      return <InfoCircleTwoTone twoToneColor="#1890ff" />;
     case "error":
-      return <CloseCircleOutlined twoToneColor="#ff4d4f" />;
+      return <CloseCircleTwoTone twoToneColor="#ff4d4f" />;
     case "warning":
-      return <ExclamationCircleOutlined twoToneColor="#faad14" />;
-    case "":
-      return "";
+      return <ExclamationCircleTwoTone twoToneColor="#faad14" />;
     default:
       return "";
-    // return <img src="{icon}" className="l-modal-box-header-icon" />;
   }
-};
-// Buttons
-const Buttons = (props) => {
-  let ConfirmBtn = () => <button>{props.confirmBtnText}</button>;
-  if (!props.showConfirmBtn) {
-    ConfirmBtn = "";
-  }
-  let CancelBtn = () => <button>{props.cancelBtnText}</button>;
-  if (!props.showCancelBtn) {
-    CancelBtn = "";
-  }
-  return (
-    <div className="btns-container">
-      <ConfirmBtn />
-      <CancelBtn />
-    </div>
-  );
 };
 
 function Modal(props) {
@@ -50,28 +31,57 @@ function Modal(props) {
   const showCancelBtn = props.showCancelBtn || true; // 是否显示取消按钮
   const confirmBtnText = props.confirmBtnText || "确定"; // 确认按钮文字
   const cancelBtnText = props.cancelBtnText || "取消"; // 取消按钮文字
+  const closeModal = () => {
+    setShow(false);
+  };
+
+  useEffect(() => {
+    setShow(props.showModal);
+  }, [props.showModal, setShow]);
   return (
-    <div className="l-modal ">
+    <div className={show ? "l-modal l-modal-show" : "l-modal"}>
+      {props.mask ? <div className="l-modal-mask"></div> : ""}
       <div className="l-modal-box">
         <div className="l-modal-box-header">
           <div className="icon-title-box">
-            <div className="icon">
-              <Icon icon={props.icon} />
-            </div>
+            {props.type ? (
+              <div className="icon">
+                <Icon type={props.type} />
+              </div>
+            ) : (
+              ""
+            )}
             <div className="title">{title}</div>
           </div>
-          <div className="close-box">
+          <div className="close-box" onClick={props.handleClose}>
             <CloseOutlined />
           </div>
         </div>
-        <div className="l-modal-box-content">{props.children}</div>
+        <div
+          className={
+            !props.type
+              ? "l-modal-box-content"
+              : "l-modal-box-content l-modal-box-content-right-move"
+          }
+        >
+          {props.children}
+        </div>
+
         <div className="l-modal-box-btns">
-          <Buttons
-            showConfirmBtn={showConfirmBtn}
-            showCancelBtn={showCancelBtn}
-            cancelBtnText={cancelBtnText}
-            confirmBtnText={confirmBtnText}
-          />
+          {showCancelBtn ? (
+            <button className="btn-confirm" onClick={props.onCancel}>
+              {cancelBtnText}
+            </button>
+          ) : (
+            ""
+          )}
+          {showConfirmBtn ? (
+            <button className="btn-cancel" onClick={props.onConfirm}>
+              {confirmBtnText}
+            </button>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import Modal from "../components/modal/index";
 
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => {
+    setShowModal(true);
+  };
+  const closeModal = () => {
+    setShowModal(false);
+  };
+  const onCancel = () => {
+    alert("点击了取消");
+  };
+  const onConfirm = () => {
+    alert("点击了确定");
+  };
   return (
     <div className="container">
       <h1>主页</h1>
-      <Modal>
-        <h2>Modal</h2>
+      <Modal
+        showModal={showModal}
+        mask={true}
+        type="info"
+        title="这是标题"
+        handleClose={closeModal}
+        onCancel={onCancel}
+        onConfirm={onConfirm}
+      >
+        这是内容
       </Modal>
+      <button onClick={openModal}>打开弹窗</button>
     </div>
   );
 };
