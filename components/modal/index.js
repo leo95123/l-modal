37ade6219a568c68a8222a032b58bdb69350e92a
lei95123 <leo95123@qq.com>
2020-12-30 10:51:42 +0800

    feat:确定参数

--- components/modal/index.js
+++ components/modal/index.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react'
-
-// 判断icon
+import {
+  CheckCircleOutlined,
+  InfoCircleOutlined,
+  CloseCircleOutlined,
+  ExclamationCircleOutlined,
+  CloseOutlined,
+} from '@ant-design/icons'
+// 获取con
 const getIcon = (icon) => {
   switch (icon) {
     case 'success':
@@ -20,17 +26,29 @@ const getIcon = (icon) => {
 const Modal = (props) => {
   const [show, setShow] = useState(props.showModal) // 是否显示
   const title = props.title || '' //标题
-  const icon = getIcon(props.icon) //图标
+  const Icon = getIcon(props.icon) //图标
   const showConfirmBtn = props.showConfirmBtn || true // 是否显示确认按钮
   const showCancelBtn = props.showCancelBtn || true // 是否显示取消按钮
   const confirmBtnText = props.confirmBtnText || '确定' // 确认按钮文字
   const cancelBtnText = props.cancelBtnText || '取消' // 取消按钮文字
   return (
-    <div className='l-modal'>
+    <div className='l-modal '>
       <div className='l-modal-box'>
-        <div className='l-modal-box-header'></div>
-        <div className='l-modal-box-content'></div>
-        <div className='l-modal-box-btns'></div>
+        <div className='l-modal-box-header'>
+          <div className='icon-title-box'>
+            <div className='icon'>
+              <Icon></Icon>
+            </div>
+            <div className='title'>{title}</div>
+          </div>
+          <div className='close-box'>
+            <CloseOutlined></CloseOutlined>
+          </div>
+        </div>
+        <div className='l-modal-box-content'>{props.children}</div>
+        <div className='l-modal-box-btns'>
+
+        </div>
       </div>
     </div>
   )
